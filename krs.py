ea188f9a1103559246a4b3215e8aae040a970742
Jacek "d33tah" Wielemborek <git@d33.pl>
2024-12-05 14:01:57 +0100

    ruff check krs.py --fix

--- krs.py
+++ krs.py
@@ -11,10 +11,10 @@ import requests
 from flask import Flask, redirect, send_from_directory
 app = Flask(__name__)
 
-KRS_URL = 'https://krs-pobierz.pl/stowarzyszenie-hakierspejs-lodz-i7113260'
+KRS_URL = "https://krs-pobierz.pl/stowarzyszenie-hakierspejs-lodz-i7113260"
 
 def get_time_since_caching():
-    cache_file = pathlib.Path('cache.txt')
+    cache_file = pathlib.Path("cache.txt")
     # if mtime is younger than 10 minutes, return hello world
     ts = datetime.datetime.fromtimestamp(cache_file.stat().st_mtime)
     elapsed = (datetime.datetime.now() - ts)
@@ -42,40 +42,40 @@ def get_krs_last_updated():
         h = lxml.html.fromstring(r.text)
         xpath = '//div [@class="lastDownloaded"]/p[2]/text()'
         last_updated = h.xpath(xpath)[0]
-        sys.stderr.write(f'{last_updated=}\n')
+        sys.stderr.write(f"{last_updated=}\n")
     except Exception:
         last_updated = cached
-        logging.exception('Error parsing krs-pobierz.pl')
+        logging.exception("Error parsing krs-pobierz.pl")
 
     # store the last_updated information in a cache file
-    with cache_file.open('w') as f:
+    with cache_file.open("w") as f:
         f.write(last_updated)
 
     return last_updated
 
 
-@app.route('/')
+@app.route("/")
 def serve_krs():
     last_updated = get_krs_last_updated()
-    if last_updated != '2024-08-07 05:19:56':
+    if last_updated != "2024-08-07 05:19:56":
         # redirect to KRS URL
         return redirect(KRS_URL)
 
-    with open('krs.html') as f:
+    with open("krs.html") as f:
         krs_cached = f.read()
 
     return krs_cached
 
 # serve *.pdf files directly
-@app.route('/<path:filename>')
+@app.route("/<path:filename>")
 def serve_pdf(filename):
-    sys.stderr.write(f'{filename=}\n')
+    sys.stderr.write(f"{filename=}\n")
     # is it a pdf file?
-    if not filename.endswith('.pdf'):
-        return '404'
-    return send_from_directory('.', filename)
+    if not filename.endswith(".pdf"):
+        return "404"
+    return send_from_directory(".", filename)
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
